048ddf99c47b7babde60477b84ca4de313ffcb53
sau226 <32805612+sau226@users.noreply.github.com>
2020-04-23 23:04:06 +0800

    Add plugin URL (#6)

--- plugin.rb
+++ plugin.rb
@@ -4,6 +4,7 @@
 # about: Show users how many characters are required before posting
 # version: 0.1
 # authors: Robin Ward
+# url: https://github.com/discourse/discourse-characters-required
 
 enabled_site_setting :characters_required_enabled
 

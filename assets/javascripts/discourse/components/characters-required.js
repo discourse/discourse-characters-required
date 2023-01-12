d9bf7c86fc5f90db77970ca3e5971371ba7bf031
Jeff Wong <awole20@gmail.com>
2023-01-11 16:24:42 -0800

    FIX: component import (#46)
    FIX: component import, add basic component test

--- assets/javascripts/discourse/components/characters-required.js
+++ assets/javascripts/discourse/components/characters-required.js
@@ -1,7 +1,7 @@
 import discourseComputed from "discourse-common/utils/decorators";
-import Ember from "ember";
+import Component from "@ember/component";
 
-export default Ember.Component.extend({
+export default Component.extend({
   classNames: ["characters-required"],
 
   @discourseComputed("composer.missingReplyCharacters")
